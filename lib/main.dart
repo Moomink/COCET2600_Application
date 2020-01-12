ed28ef0028b717fd93e949d2b95bd3aceddeb8bf
Moomink <kasutamu907@gmail.com>
2020-01-13 00:55:41 +0900

    ・単語までの50単語ずつの目次の外装の制作

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:cocet_2600/show_words.dart';
-import 'package:cocet_2600/sidebar.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(COCET());
 
 class COCET extends StatefulWidget {
-  static int start = 0;
-  static int end = 0;
+  static int start = 1;
+  static int end = 500;
+
   @override
   AppState createState() => AppState();
 }
@@ -21,11 +21,92 @@ class AppState extends State<COCET> {
             centerTitle: true,
             title: Text('COCET 2600'),
           ),
-          drawer: Drawer(child: Sidebar()),
+          drawer: Drawer(child: sideBar()),
           body: wordList(COCET.start, COCET.end),
-        )
-    );
+        ));
   }
 
+  Widget sideBar() {
+    debugPrint("SideBar");
+    return Container(
+        child: Column(children: <Widget>[
+      Row(children: <Widget>[
+        Expanded(
+            child: DrawerHeader(
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5),
+              color: Colors.blue,
+              image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage('assets/images/NITKC.jpg'))),
+          child: Text('COCET 2600',
+              style: TextStyle(color: Colors.black, fontSize: 24),
+              textAlign: TextAlign.center),
+        ))
+      ]),
+      Expanded(
+          child: ListView(children: <Widget>[
+        Card(
+          color: Colors.cyanAccent,
+          child: ListTile(
+              title: Text('No.0001 ～ No.0500',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.black, fontSize: 24)),
+              onTap: () {
+                showList(1, 500);
+              }),
+        ),
+        Card(
+          color: Colors.cyanAccent,
+          child: ListTile(
+            title: Text('No.0501 ～ No.1000',
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.black, fontSize: 24)),
+            onTap: () {
+              showList(501, 1000);
+            },
+          ),
+        ),
+        Card(
+          color: Colors.cyanAccent,
+          child: ListTile(
+            title: Text('No.1001 ～ No.1500',
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.black, fontSize: 24)),
+            onTap: () {
+              showList(1001, 1500);
+            },
+          ),
+        ),
+        Card(
+          color: Colors.cyanAccent,
+          child: ListTile(
+            title: Text('No.1501 ～ No.2000',
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.black, fontSize: 24)),
+            onTap: () {
+              showList(1501, 2000);
+            },
+          ),
+        ),
+        Card(
+          color: Colors.cyanAccent,
+          child: ListTile(
+            title: Text('No.2001 ～ No.2600',
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.black, fontSize: 24)),
+            onTap: () {
+              showList(2001, 2600);
+            },
+          ),
+        ),
+      ]))
+    ]));
+  }
 
+  void showList(int start, int end) {
+    COCET.start = start;
+    COCET.end = end;
+    setState(() {});
+  }
 }
--- lib/show_words.dart
+++ lib/show_words.dart
@@ -4,15 +4,22 @@ import 'package:flutter/material.dart';
 
 Widget wordList(int rangeStart, int rangeEnd) {
   int list = (rangeEnd - rangeStart + 1) ~/ 50;
+  int i = -1;
   // ignore: missing_return
-  return ListView.builder(itemBuilder: (context, tmp) {
-    for (int i = 0; i < list; i++) {
-      Card(
-          color: Colors.cyanAccent,
-          child: Text(
-            'No.${rangeStart + i * 50} ～ No.${rangeStart + i * 50 + 50}',
-            style: TextStyle(color: Colors.black38, fontSize: 20),
-          ));
+  return ListView.builder(itemBuilder: (context, index) {
+    if (index < list) {
+      i++;
+      return _buildList(rangeStart + i * 50, rangeStart + i * 50 + 50 - 1);
     }
   });
+}
+
+Widget _buildList(int start, int end) {
+  return Card(
+      color: Colors.cyanAccent,
+      child: Text(
+        'No.$start ～ No.$end',
+        style: TextStyle(color: Colors.black, fontSize: 30),
+        textAlign: TextAlign.center,
+      ));
 }
